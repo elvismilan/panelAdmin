7ddb29b91b6a18cdc763603d3727d8117c27e52e
Elvis Milan <milan.elvis@gmail.com>
2024-04-24 14:34:49 -0400

    cleaning assets

--- src/Controller.php
+++ src/Controller.php
@@ -16,6 +16,13 @@ class Controller
                 include "Views/$view.php";
                 break;
 
+            case 'dashboard' : 
+
+                require_once 'Resources/layouts/partials/header.php';
+                include "Views/$view.php";
+                require_once 'Resources/layouts/partials/footer.php';
+                break;
+
             default:
 
                 include "Views/$view.php";
